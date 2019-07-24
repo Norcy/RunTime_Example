2e5190aa07662a86e19704b24f90ea3d9f00786f
Norcy <Norcy@users.noreply.github.com>
2019-07-24 11:33:02 +0800

    Update NSObject+Swizzle.h

--- NSObject+Swizzle.h
+++ NSObject+Swizzle.h
@@ -10,6 +10,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+// swizzleMethod 采用 C 函数，而不是 NSObject 的方法，是为了防止子类在 load 方法中向其自己发送消息
+// 那样会导致其 +initialize 方法在 load 的时候被提前调用，而此时系统环境是不稳定的
 void swizzleMethod(Class class, SEL originalSelector, SEL swizzledSelector);
 
 @interface NSObject (Swizzle)
